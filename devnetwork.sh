bccbcb9ba23bdf6ab71703b2aaaa4adb9e1a1737
agungdhewe <agung_dhewe@yahoo.com>
2025-01-02 05:52:37 +0700

    setup network

--- devnetwork.sh
+++ devnetwork.sh
@@ -1,2 +1,2 @@
-docker network create --driver=bridge --subnet=192.168.15.0/24 --gateway=192.168.15.1 devnetwork
+docker network create --driver=bridge --subnet=192.168.15.0/24 --gateway=192.168.15.1 --opt com.docker.network.bridge.name=devnetwork devnetwork
 
